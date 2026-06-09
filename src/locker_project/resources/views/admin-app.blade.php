19e7e9699efab7cf98aeb1cd1f1d178a31b86dd6
1amrzy <1amrzy@users.noreply.github.com>
2026-06-09 14:19:41 +0700

    attachment logo

--- src/locker_project/resources/views/admin-app.blade.php
+++ src/locker_project/resources/views/admin-app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Admin Dashboard - LockER</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/admin/main-admin.jsx'])
 </head>
